685068c3d79c2634531e5e6aaac05fa025cd69be
69010983 <69010983@kmitl.ac.th>
2026-09-23 14:43:19 +0700

    added comment in checkin function

--- hello.c
+++ hello.c
@@ -18,6 +18,7 @@ int greeting(const char *name)
 }
 int checkin(const char *name)
 {
+    // Print a check in message for the specified user
     printf("%s are checked in\n", name);
     return 0;
 }
