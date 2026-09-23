6887adb04ad3a2f6c14c9577d62cf635bcaac3bc
69010983 <69010983@kmitl.ac.th>
2026-09-23 14:35:10 +0700

    add check in func

--- hello.c
+++ hello.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-int farewell(const char *name)// Print a farewell message for the specified user
+int farewell(const char *name) // Print a farewell message for the specified user
 {
     printf("Goodbye, %s!\n", name);
     return 0;
 }
 int main(void)
 {
-    greeting("Developer A,B,C !!");
-    farewell("Developer A,B,C");
+    checkin("Developer A, B, C");
+    greeting("Developer A, B, C !!");
+    farewell("Developer A, B, C");
     return 0;
 }
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name); // Print a greeting message for the specified user
+    return 0;
+}
+int checkin(const char *name)
+{
+    printf("%s are checked in\n", name);
     return 0;
 }
